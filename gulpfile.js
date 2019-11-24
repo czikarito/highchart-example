6e8d1b5948fdea5d8c56fece87c1e92d4610d0ad
Paweł Kuwik <p.kuwik@selleo.com>
2019-11-24 20:50:27 +0100

    feat: image resizer

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var conventionalChangelog = require('gulp-conventional-changelog');
+const imageResize = require('gulp-image-resize');
 
 gulp.task('changelog', function () {
   return gulp.src('CHANGELOG.md')
@@ -8,3 +9,14 @@ gulp.task('changelog', function () {
     }))
     .pipe(gulp.dest('./'));
 });
+
+gulp.task('resizer', function() {
+  return gulp
+    .src('source/images/*.{jpg,jpeg}')
+    .pipe(imageResize({
+      width : 500,
+      height : 500,
+      crop : true
+    }))
+    .pipe(gulp.dest('public/images/'))
+  });
