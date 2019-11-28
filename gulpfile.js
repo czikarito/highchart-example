6e7192da875dfdc044a925350aa5e5682bf74360
Paweł Kuwik <p.kuwik@selleo.com>
2019-11-28 14:44:21 +0100

    feat: generator

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+var conventionalChangelog = require('gulp-conventional-changelog');
+
+gulp.task('changelog', function () {
+  return gulp.src('CHANGELOG.md')
+    .pipe(conventionalChangelog({
+      preset: 'angular'
+    }))
+    .pipe(gulp.dest('./'));
+});
